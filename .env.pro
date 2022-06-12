b1ef2e5f4ad830adda827d4954bdb19d8e9c2fd1
wangdagang <961516491@qq.com>
2022-06-12 22:38:58 +0800

    多环境配置

---
+++ .env.pro
@@ -0,0 +1,5 @@
+# 生产环境配置
+VITE_NODE_ENV = pro
+# 环境配置
+VITE_BASE_URL = 'http://localhost:8080'
+VITE_BASE_API = '/pro-api'
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
@@ -10,52 +10,59 @@ import { resolve } from 'path'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    /**
-     * elementui-plus 按需引入
-     */
-     Components({
-      // allow auto load markdown components under `./src/components/`
-      extensions: ['vue', 'md'],
-      // allow auto import and register components used in markdown
-      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-      resolvers: [
-        ElementPlusResolver({
-          importStyle: 'sass',
-        }),
-      ],
-      dts: 'src/components.d.ts',
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src') // 设置 `@` 指向 `src` 目录
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "@/styles/element/index.scss" as *;`,
+export default defineConfig(({ command, mode }) => {
+  /**环境变量 */
+  const env = loadEnv(mode, __dirname)
+
+  return {
+    plugins: [
+      vue(),
+      /**
+       * elementui-plus 按需引入
+       */
+      Components({
+        // allow auto load markdown components under `./src/components/`
+        extensions: ['vue', 'md'],
+        // allow auto import and register components used in markdown
+        include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+        resolvers: [
+          ElementPlusResolver({
+            importStyle: 'sass',
+          }),
+        ],
+        dts: 'src/components.d.ts',
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src') // 设置 `@` 指向 `src` 目录
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@/styles/element/index.scss" as *;`,
+        },
       },
     },
-  },
-  base: './', // 设置打包路径
-  server: {
-    port: 4000, // 设置服务启动端口号
-    open: true, // 设置服务启动时是否自动打开浏览器
-    cors: true // 允许跨域
+    envPrefix: ["VITE"],
+    base: './', // 设置打包路径
+    server: {
+      port: 4000, // 设置服务启动端口号
+      open: true, // 设置服务启动时是否自动打开浏览器
+      cors: true, // 允许跨域
 
-    // 设置代理，根据我们项目实际情况配置
-    // proxy: {
-    //   '/api': {
-    //     target: 'http://xxx.xxx.xxx.xxx:8000',
-    //     changeOrigin: true,
-    //     secure: false,
-    //     rewrite: (path) => path.replace('/api/', '/')
-    //   }
-    // }
+      // 设置代理，根据我们项目实际情况配置
+      proxy: {
+        [env.VITE_BASE_API]: {
+          target: env.VITE_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(env.VITE_BASE_API, '/')
+        }
+      }
+    }
   }
 })
